f3fef662d434b7d810207654610b02aed401c95e
Ravi <nuggehalliravi@gmail.com>
2025-06-20 15:52:42 +0530

    Test

--- Test_calculator.py
+++ Test_calculator.py
@@ -4,13 +4,13 @@ from Calculator import add, subtract, multiply, divide
 import pytest
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(5, 3) == 5
 
 def test_subtract():
-    assert subtract(10, 5) == 5
+    assert subtract(10, 1) == 5
 
 def test_multiply():
-    assert multiply(2, 4) == 8
+    assert multiply(2, 7) == 8
 
 def test_divide():
     assert divide(10, 2) == 5
